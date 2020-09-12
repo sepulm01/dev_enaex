6b3adc885be886649a79418648f342b413958f1d
sepulm01 <rguax910>
2020-09-12 11:08:50 -0300

    settings

--- mysite/mysite/settings.py
+++ mysite/mysite/settings.py
@@ -133,14 +133,16 @@ USE_TZ = True
 # https://docs.djangoproject.com/en/3.0/howto/static-files/
 
 STATIC_URL = '/static/'
+MEDIA_URL = '/media/'
 
 STATICFILES_DIRS = [
     os.path.join(BASE_DIR, "static"),
     '/var/www/static/',
 ]
-
-MEDIA_URL = '/media/'
 MEDIA_ROOT = os.path.join(BASE_DIR, 'media')
+
+#STATIC_ROOT = '/var/www/mysite/static/'
+#MEDIA_ROOT = '/var/www/mysite/media/'
 
 EMAIL_BACKEND = 'django.core.mail.backends.smtp.EmailBackend'
 EMAIL_USE_TLS = True
@@ -152,3 +154,9 @@ EMAIL_HOST_PASSWORD = ''
 DEFAULT_FROM_EMAIL = EMAIL_HOST_USER
 
 LOGIN_REDIRECT_URL = '/'
+
+STATIC_URL = '/static/'
+
+
+
+
