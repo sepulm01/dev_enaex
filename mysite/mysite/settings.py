17970834f996736bffc95de4dccd9566f8a33016
sepulm01 <rguax910>
2020-12-12 20:21:40 -0300

    db

--- mysite/mysite/settings.py
+++ mysite/mysite/settings.py
@@ -89,23 +89,23 @@ WSGI_APPLICATION = 'mysite.wsgi.application'
 # Database
 # https://docs.djangoproject.com/en/3.0/ref/settings/#databases
 
-DATABASES = {
-    'default': {
-        'ENGINE': 'django.db.backends.sqlite3',
-        'NAME': os.path.join(BASE_DIR, 'db.sqlite3'),
-    }
-}
-
 # DATABASES = {
-#         'default': {
-#             'ENGINE': 'django.db.backends.postgresql_psycopg2',
-#             'NAME': 'camaras', 
-#             'USER': 'postgres',
-#             'PASSWORD': 'mysecretpassword', 
-#             'HOST': os.getenv('DB_HOST', '172.17.0.2' ),
-#             'PORT': os.getenv('DB_PORT', '5432')
-#         }
+#     'default': {
+#         'ENGINE': 'django.db.backends.sqlite3',
+#         'NAME': os.path.join(BASE_DIR, 'db.sqlite3'),
+#     }
 # }
+
+DATABASES = {
+        'default': {
+            'ENGINE': 'django.db.backends.postgresql_psycopg2',
+            'NAME': 'camaras', 
+            'USER': 'postgres',
+            'PASSWORD': 'mysecretpassword', 
+            'HOST': os.getenv('DB_HOST', '172.17.0.2' ),
+            'PORT': os.getenv('DB_PORT', '5432')
+        }
+}
 
 
 # Password validation
